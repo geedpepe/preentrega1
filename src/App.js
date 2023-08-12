b66ec5f36a06c6ddd272f6ea4e2fc129fa6b689f
geedpepe <82737456+geedpepe@users.noreply.github.com>
2023-08-12 18:45:27 -0300

    entrega1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from './Components/CartWidget';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import ItemListContainer from './Components/ItemListContainer';
+
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark"> {/* Use Bootstrap bg and variant props */}
+    <div>
+    <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Tienda Muebles Uruguay</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -15,19 +22,24 @@ function BasicExample() {
             <Nav.Link href="#link">Galeria</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Presupuestos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Trabajos
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Trabajos</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Garantia</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Social media
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Social media</NavDropdown.Item>
             </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#cart">
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+      <Container>
+      <ItemListContainer />
+    </Container>
+  </div>
   );
 }
 
---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ItemListContainer() {
+  return (
+    <div>
+      <h2>Articulos</h2>
+      <ul>
+        <li>Mesa</li>
+        <li>Sillon</li>
+        <li>Rack</li>
+        {/* Agrega más elementos de lista según sea necesario */}
+      </ul>
+    </div>
+  );
+}
+
+export default ItemListContainer;
