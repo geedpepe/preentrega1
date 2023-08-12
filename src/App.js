b70818a7982d4a1ef7b6355fb33a5a749657c16b
geedpepe <82737456+geedpepe@users.noreply.github.com>
2023-08-12 19:10:07 -0300

    ultimo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import CartWidget from './Components/CartWidget';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import ItemListContainer from './Components/ItemListContainer';
